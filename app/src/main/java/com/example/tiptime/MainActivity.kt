0bb59e3c10e3968a4d57b1ae8bc57afd124b9f8c
Bosh <60085635+ThePurpleClub@users.noreply.github.com>
2024-01-29 14:32:46 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -55,7 +55,6 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.tiptime.R
 import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
 
