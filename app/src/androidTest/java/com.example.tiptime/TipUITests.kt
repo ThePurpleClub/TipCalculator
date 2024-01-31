444c6f88aee0ec1af16ac5313dfe95a97c4a0095
Bosh <60085635+ThePurpleClub@users.noreply.github.com>
2024-01-31 10:46:31 +0000

    writing instrumentation test
    writing instrumentation test

---
+++ app/src/androidTest/java/com.example.tiptime/TipUITests.kt
@@ -0,0 +1,16 @@
+import org.junit.Rule
+import org.junit.Test
+
+class TipUITests {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent{
+            TipTimeTheme{
+                TipTimeLayout()
+            }
+        }
+    }
+}
